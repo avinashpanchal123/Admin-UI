05a9bd85c2c2d1a07f7c8cc03c2e607fa25053ee
Avinash Panchal <88651615+avinashpanchal123@users.noreply.github.com>
2022-04-03 12:42:03 +0530

    created React basic setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Pagination from "@material-ui/lab/Pagination";
+
+function App() {
+  return (
+    <>
+     <h1>hello I am app</h1>
+    </>
+  );
+}
+
+export default App;
